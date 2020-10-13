aaac2f77148b717d1435e02c03aa15920040a3f1
Odile078 <ouwimpuhwe620@daviscollege.com>
2020-10-13 15:23:24 +0200

    changed images folder

---
+++ JS/index.js
@@ -0,0 +1,26 @@
+//slideshow
+var slideshows = document.querySelectorAll('[data-component="slideshow"]');
+  
+  // Apply to all slideshows defined
+  slideshows.forEach(initSlideShow);
+
+  function initSlideShow(slideshow) {
+
+    var slides = document.querySelectorAll(`#${slideshow.id} [role="list"] .slide`); // Get an array of slides
+
+    var index = 0, time = 5000;
+    slides[index].classList.add('active');  
+    
+    setInterval( () => {
+      slides[index].classList.remove('active');
+      
+      //Go over each slide incrementing the index
+      index++;
+      
+      //  restart the index to show the first slide and start again
+      if (index === slides.length) index = 0; 
+      
+      slides[index].classList.add('active');
+
+    }, time);
+  }
